d20187583bb1717fa0832eb3eb7ef16076779f7e
LeoNunes <leonardonunesdev@gmail.com>
2023-06-04 01:34:29 -0300

    Initial commit

---
+++ test/rooks_and_walls_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RooksAndWallsCdk from '../lib/rooks_and_walls_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/rooks_and_walls_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RooksAndWallsCdk.RooksAndWallsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
